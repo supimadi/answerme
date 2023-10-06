555ab16fa5dacd0676a2b2ac21c950199ffb4aef
Lukman Hakim <hlukman156@gmail.com>
2023-10-06 21:06:29 +0700

    add onboarding activity to the app

--- app/src/main/java/org/d3if3038/answerme/MainActivity.kt
+++ app/src/main/java/org/d3if3038/answerme/MainActivity.kt
@@ -1,5 +1,6 @@
 package org.d3if3038.answerme
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startActivity(Intent(this, OnBoardingActivity::class.java))
     }
 }
---
+++ app/src/main/java/org/d3if3038/answerme/OnBoardingActivity.kt
@@ -0,0 +1,71 @@
+package org.d3if3038.answerme
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.Window
+import androidx.fragment.app.Fragment
+import com.github.appintro.AppIntro2
+import com.github.appintro.AppIntroFragment
+import com.github.appintro.AppIntroPageTransformerType
+
+class OnBoardingActivity : AppIntro2() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
+        super.onCreate(savedInstanceState)
+
+        // Set flags for onboarding screen
+        isSystemBackButtonLocked = true
+        isColorTransitionsEnabled = true
+
+        setImmersiveMode()
+        showStatusBar(true)
+
+        // Create parallax effect
+        setTransformer(AppIntroPageTransformerType.Parallax(
+            titleParallaxFactor = 1.0,
+            imageParallaxFactor = -1.0,
+            descriptionParallaxFactor = 2.0
+        ))
+
+        addSlide(createSlide(
+            "Selamat Datang",
+            "Hai kamu, saatnya memulai obrolan asik dan seru bersama AnswerMe.",
+            R.drawable.welcome_art
+        ))
+        addSlide(createSlide(
+            "AnswerMe ?",
+            "Kamu dapat mengirim pesan anonym \n" +
+                    "pada orang lain tanpa diketahui, kamu juga bisa \n" +
+                    "balas pesan orang lain secara anonym, \n" +
+                    "ayo coba sekarang!,",
+            R.drawable.masrsmellow_art
+        ))
+        addSlide(createSlide(
+            "Woo Hoo!",
+            "Kamu hampir sampai, ayo kita mulai!",
+            R.drawable.feedback_art
+        ))
+    }
+
+    private fun createSlide(title: String, desc: String, image: Int) : Fragment {
+        return AppIntroFragment.createInstance(
+            title = title,
+            description = desc,
+            imageDrawable = image,
+            titleColorRes = R.color.black,
+            descriptionColorRes = R.color.black,
+        )
+    }
+
+    override fun onSkipPressed(currentFragment: Fragment?) {
+        super.onSkipPressed(currentFragment)
+        finish()
+    }
+
+    override fun onDonePressed(currentFragment: Fragment?) {
+        super.onDonePressed(currentFragment)
+        finish()
+    }
+}
